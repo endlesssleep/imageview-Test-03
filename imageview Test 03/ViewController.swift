5fa4a7002ec73d360bfc5888134089af62e71f0f
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-26 15:12:21 +0900

    02

--- imageview Test 03/ViewController.swift
+++ imageview Test 03/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "frame1.png")
+        
     }
-
-
+    @IBAction func changImage(_ sender: Any) {
+        if count == 5 {
+            count = 0
+        }
+        
+        
+        count = count + 1
+        myImageView.image = UIImage(named: "frame\(count).png")
+    }
+    
 }
 
